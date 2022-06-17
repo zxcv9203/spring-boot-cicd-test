4aba6156e2fc7a0554fc5654ce63e711f1cba83e
yongckim <zxcv9203@naver.com>
2022-06-17 22:30:27 +0900

    config : ci/cd start shell script typing error modify

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,13 +4,12 @@ PROJECT_NAME="demo"
 PROJECT_VERSION="0.0.1-SNAPSHOT"
 JAR_FILE="$PROJECT_PATH/target/$PROJECT_NAME-$PROJECT_VERSION"
 
-DEPLOY_LOG="$PROJECT_PATH/logs/deploy.log"
+DEPLOY_LOG="$PROJECT_PATH/deploy.log"
 
+NOW=$(date +%c)
 
-NOW = $(date +%c)
-
-echo "[$NOW] 애플리케이션 실행" >> $DEPLOY_LOG
+echo "[$NOW] 애플리케이션 실행" >>$DEPLOY_LOG
 nohup java -jar $JAR_FILE &
 
 RUNNING_PID=$(pgrep -f $JAR_FILE)
-echo "[$NOW] 프로세스가 정상적으로 실행되었습니다. 실행 PID -> $RUNNING_PID" >> $DEPLOY_LOG
+echo "[$NOW] 프로세스가 정상적으로 실행되었습니다. 실행 PID -> $RUNNING_PID" >>$DEPLOY_LOG
