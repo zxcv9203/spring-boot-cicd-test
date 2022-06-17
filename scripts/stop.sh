eb6fe0a17585de6601d810ab6eddfd629efcb79b
yongckim <zxcv9203@naver.com>
2022-06-17 22:22:13 +0900

    config : ci/cd stop & start shell script typing error modify

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -9,7 +9,7 @@ RUNNING_PID=$(pgrep -f $JAR_FILE)
 
 NOW=$(date +%c)
 
-if [-z $CUREENT_PID]; then
+if [ -z $CUREENT_PID ]; then
   echo "[$NOW] 현재 실행중인 애플리케이션이 없습니다." >>$DEPLOY_LOG
 else
   echo "[$NOW] 실행중인 $RUNNING_PID 애플리케이션을 종료합니다." >> $DEPLOY_LOG
